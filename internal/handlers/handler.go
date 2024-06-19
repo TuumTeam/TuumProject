cd932adee87af63e271dc5aaac7d00eaaa2c3704
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-06-19 14:57:07 +0200

    handling users

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -1,8 +1,12 @@
 package handlers
 
 import (
+	"database/sql"
 	"html/template"
+	"log"
 	"net/http"
+	"net/url"
+	"tuum.com/internal/repositories"
 
 	"github.com/gorilla/sessions"
 )
@@ -27,11 +31,41 @@ func RedirectToTuum(w http.ResponseWriter, r *http.Request) {
 }
 
 func RegisterToLogin(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("web/templates/login.html"))
-	err := tmpl.Execute(w, nil)
+	if r.Method != http.MethodPost {
+		tmpl := template.Must(template.ParseFiles("/web/templates/login.html"))
+		err := tmpl.Execute(w, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	// Handle login
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+
+	// Create an instance of UserHandler
+	db, err := sql.Open("sqlite3", "/data/forum.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	userRepo := repositories.NewUserRepository(db)
+	userHandler := NewUserHandler(userRepo)
+
+	// Create a new request with the email and password
+	loginReq, err := http.NewRequest("POST", "/login", nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+	loginReq.Form = url.Values{
+		"email":    {email},
+		"password": {password},
+	}
+
+	// Call the Login method of UserHandler
+	userHandler.Login(w, loginReq)
 }
 
 func RedirectToRegister(w http.ResponseWriter, r *http.Request) {
