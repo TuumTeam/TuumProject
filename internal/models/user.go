7a45e942c4dc8aa205f30053b620b0e8c87da4aa
nvtnicolas <nicolas.nguyenvanthanh@ynov.com>
2024-06-19 15:09:10 +0200

    create Hash for password in data base

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,1 +1,25 @@
 package models
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID       int
+	Username string
+	Password string
+}
+
+func (u *User) HashPassword() error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+	return nil
+}
+
+func (u *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
+}
