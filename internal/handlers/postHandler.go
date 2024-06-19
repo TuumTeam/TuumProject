20b94d6a281621695d168778f0241175b33183bf
nvtnicolas <nicolas.nguyenvanthanh@ynov.com>
2024-06-19 15:07:35 +0200

    change post Handler

--- internal/handlers/postHandler.go
+++ internal/handlers/postHandler.go
@@ -1,1 +1,15 @@
 package handlers
+
+import (
+	"net/http"
+)
+
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		// Gestion des erreurs ici
+		http.Error(w, "Not implemented", http.StatusNotImplemented)
+	default:
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+}
