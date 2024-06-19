de435cfcfb43452297cfa82dd793005660c6b3a3
nvtnicolas <nicolas.nguyenvanthanh@ynov.com>
2024-06-19 15:07:06 +0200

    change main for https

--- cmd/forum/main.go
+++ cmd/forum/main.go
@@ -1,1 +1,20 @@
-package forum
+package main
+
+import (
+	"log"
+	"net/http"
+	"tuum.com/internal/config"
+	"tuum.com/internal/handlers"
+)
+
+func main() {
+	tlsConfig := config.SetupTLSConfig()
+
+	server := &http.Server{
+		Addr:      ":https",
+		Handler:   handlers.NewRouter(),
+		TLSConfig: tlsConfig,
+	}
+
+	log.Fatal(server.ListenAndServeTLS("", ""))
+}
