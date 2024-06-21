deff34237851a38b637cef61fe1e95d2ffcd1fed
nvtnicolas <nicolas.nguyenvanthanh@ynov.com>
2024-06-21 14:26:22 +0200

    correct the merge and add path test for certificate and key

--- cmd/forum/main.go
+++ cmd/forum/main.go
@@ -1,28 +1,32 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"tuum.com/internal/config"
-	"tuum.com/internal/handlers"
-	"database/sql"
 	"github.com/gorilla/csrf"
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"tuum.com/internal/handlers"
-	"tuum.com/internal/repositories"
-
 )
 
 func main() {
-	tlsConfig := config.SetupTLSConfig()
+	r := mux.NewRouter()
 
-	server := &http.Server{
-		Addr:      ":https",
-		Handler:   handlers.NewRouter(),
-		TLSConfig: tlsConfig,
+	// Gestion des fichiers statiques
+	fs := http.FileServer(http.Dir("./web/static"))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
+
+	// Routes
+	r.HandleFunc("/", handlers.RedirectToIndex)
+	r.HandleFunc("/home", handlers.RedirectToIndex)
+	r.Handle("/web/protected/admin.html", handlers.IsAuthenticated(http.HandlerFunc(handlers.ProtectedFileHandler)))
+
+	// Middleware CSRF protection
+	csrfMiddleware := csrf.Protect([]byte("32-byte-long-auth-key"))
+
+	// Démarrer le serveur
+	log.Println("Serveur démarré sur : http://localhost:8080")
+	err := http.ListenAndServeTLS(":8080", `C:\\Users\\nicol\\OneDrive\\Documents\\certificate.crt`, `C:\\Users\\nicol\\OneDrive\\Documents\\private.key`, csrfMiddleware(r))
+	if err != nil {
+		log.Fatal("Erreur lors du démarrage du serveur : ", err)
 	}
-
-	log.Fatal(server.ListenAndServeTLS("", ""))
 }
