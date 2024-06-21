3c6791aa9c81bcea481c462d0b8ec36d06c5b54b
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-06-21 14:34:34 +0200

    register handler removed

--- cmd/forum/main.go
+++ cmd/forum/main.go
@@ -13,19 +13,19 @@ func main() {
 
 	fs := http.FileServer(http.Dir("./web/static"))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
+
 	r.HandleFunc("/", handlers.RedirectToIndex)
 	r.HandleFunc("/tuums", handlers.RedirectToTuums)
 	r.HandleFunc("/login", handlers.RedirectToLogin)
-	r.HandleFunc("/register", handlers.RegisterToRegister)
 
 	s := r.PathPrefix("/").Subrouter()
 	s.Use(middleware.AuthMiddleware)
 	s.HandleFunc("/profile", handlers.RedirectToProfile)
 	s.HandleFunc("/create", handlers.RedirectToCreate)
 
-	fmt.Printf("Server starting at http://localhost:8080...\n")
+	fmt.Println("Server starting at http://localhost:8080...")
 	err := http.ListenAndServe(":8080", r)
 	if err != nil {
-		return
+		fmt.Println(err)
 	}
 }
