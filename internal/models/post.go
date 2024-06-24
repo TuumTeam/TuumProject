d8ed4fe3a7877c32d0c429aa9d8fb6df61aeb0f5
rodove-tv <enzo.martinez.travail@gmail.com>
2024-06-24 15:26:56 +0200

    adding hashtags not obligatory

--- internal/models/post.go
+++ internal/models/post.go
@@ -3,6 +3,7 @@ package models
 type Post struct {
 	Title    string    `json:"title" validate:"required,min=3,max=32"`
 	Content  string    `json:"content" validate:"required"`
+	Hashtag  string    `json:"hashtag" omitempty:"null"`
 	User     User      `json:"user" validate:"required"`
 	Comments []Comment `json:"comments" validate:"required"`
 }
