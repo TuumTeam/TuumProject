895a7bdc90ebac48f3ede24c2c9e2cfbacb9b3f0
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-06-24 16:47:30 +0200

    users handlers moved to dedicated file

--- internal/handlers/userHandler.go
+++ internal/handlers/userHandler.go
@@ -1,68 +1,102 @@
 package handlers
 
 import (
-	"encoding/json"
+	"errors"
 	"net/http"
+	"time"
+	"tuum.com/internal/auth"
+	"tuum.com/internal/database"
 	"tuum.com/internal/models"
-	"tuum.com/internal/repositories"
 )
 
-// UserHandler gère les requêtes liées aux utilisateurs.
-type UserHandler struct {
-	Repo *repositories.UserRepository
+func RedirectToLogin(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		ExecTmpl(w, r, "web/templates/register.html", nil)
+	} else {
+		if r.FormValue("LogType") == "Login" {
+			logBool, _ := database.Login(r.FormValue("email"), r.FormValue("password"))
+			if logBool {
+				// Generate JWT
+				token, err := auth.GenerateJWT(r.FormValue("username"), r.FormValue("email"), "dark")
+				if err != nil {
+					http.Error(w, "Failed to generate token", http.StatusInternalServerError)
+					return
+				}
+
+				// Set JWT as cookie
+				http.SetCookie(w, &http.Cookie{
+					Name:     "session_token",
+					Value:    token,
+					Expires:  time.Now().Add(24 * time.Hour),
+					HttpOnly: true,
+				})
+				http.Redirect(w, r, "/", http.StatusSeeOther)
+			} else {
+				ExecTmpl(w, r, "web/templates/register.html", "Invalid email or password")
+			}
+		} else {
+			user := models.User{
+				ID:       0,
+				Username: r.FormValue("username"),
+				Email:    r.FormValue("email"),
+				Password: r.FormValue("password"),
+			}
+			err := database.AddUser(user)
+			if err != nil {
+				http.Error(w, "Unable to add user to database", http.StatusInternalServerError)
+				return
+			}
+
+			// Generate JWT
+			token, err := auth.GenerateJWT(r.FormValue("username"), r.FormValue("email"), "dark")
+			if err != nil {
+				http.Error(w, "Failed to generate token", http.StatusInternalServerError)
+				return
+			}
+
+			// Set JWT as cookie
+			http.SetCookie(w, &http.Cookie{
+				Name:     "session_token",
+				Value:    token,
+				Expires:  time.Now().Add(24 * time.Hour),
+				HttpOnly: true,
+			})
+
+			http.Redirect(w, r, "/profile", http.StatusSeeOther)
+		}
+	}
 }
 
-// NewUserHandler crée un nouveau UserHandler.
-func NewUserHandler(repo *repositories.UserRepository) *UserHandler {
-	return &UserHandler{Repo: repo}
-}
-
-// Signup gère l'inscription des nouveaux utilisateurs.
-func (h *UserHandler) Signup(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return
-	}
-
-	var user models.User
-	err := json.NewDecoder(r.Body).Decode(&user)
+func RedirectToProfile(w http.ResponseWriter, r *http.Request) {
+	// Extract session token from cookies
+	cookie, err := r.Cookie("session_token")
 	if err != nil {
-		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		if errors.Is(err, http.ErrNoCookie) {
+			// If the session cookie is not set, redirect to login
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+			return
+		}
+		// For any other type of error, return a bad request status
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	err = h.Repo.Create(&user)
+	// Validate JWT
+	claims, err := auth.ValidateJWT(cookie.Value)
 	if err != nil {
-		http.Error(w, "Failed to create user", http.StatusInternalServerError)
+		// If there is an error in getting the cookie, return an internal server error
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	w.WriteHeader(http.StatusCreated)
-}
-
-// Login gère la connexion des utilisateurs.
-func (h *UserHandler) Login(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return
-	}
-
-	var credentials struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}
-	err := json.NewDecoder(r.Body).Decode(&credentials)
+	// Get user details from the database
+	user, err := database.GetUserByEmail(claims.Email)
 	if err != nil {
-		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		// If there is an error in getting the user, return an internal server error
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	user, err := h.Repo.FindByEmail(credentials.Email)
-	if err != nil || !repositories.CheckPasswordHash(credentials.Password, user.Password) {
-		http.Error(w, "Invalid email or password", http.StatusUnauthorized)
-		return
-	}
-
-	// Générer et renvoyer un token JWT (non implémenté ici)
-	w.WriteHeader(http.StatusOK)
+	// Execute the profile template with the user data
+	ExecTmpl(w, r, "web/templates/profile.html", user)
 }
