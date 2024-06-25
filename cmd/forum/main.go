48dc685816b07d8e4a187bda1298893e9e195f94
nvtnicolas <nicolas.nguyenvanthanh@ynov.com>
2024-06-25 15:18:49 +0200

    little change

--- cmd/forum/main.go
+++ cmd/forum/main.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"tuum.com/internal/config" // Corrected import path
+	"tuum.com/internal/config"
 	"tuum.com/internal/handlers"
 	"tuum.com/pkg/middleware"
 )
@@ -22,12 +22,13 @@ func main() {
 
 	s := r.PathPrefix("/").Subrouter()
 	s.Use(middleware.AuthMiddleware)
+	s.Use(middleware.RateLimiter) // Add the rate limiter middleware
 	s.HandleFunc("/profile", handlers.RedirectToProfile)
 
 	// Middleware CSRF protection
 	csrfMiddleware := csrf.Protect([]byte("32-byte-long-auth-key"))
 
-	tlsConfig := config.SetupTLSConfig() // Corrected function call
+	tlsConfig := config.SetupTLSConfig()
 	// Create a custom server with the TLS configuration
 	server := &http.Server{
 		Addr:      ":443",
@@ -37,7 +38,7 @@ func main() {
 
 	// Démarrer le serveur
 	log.Println("Serveur démarré sur : https://localhost:443")
-	err := server.ListenAndServeTLS("", "")
+	err := server.ListenAndServeTLS("key/localhost.crt", "key/localhost.key")
 	if err != nil {
 		log.Fatal("Erreur lors du démarrage du serveur : ", err)
 	}
