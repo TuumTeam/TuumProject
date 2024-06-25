9a6f2836be0e720d4d1fb1be8c20ada41fff39d5
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-06-25 14:12:24 +0200

    redirecting to login page

--- pkg/middleware/authMiddleware.go
+++ pkg/middleware/authMiddleware.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"net/http"
 	"tuum.com/internal/auth"
-	"tuum.com/internal/handlers"
 )
 
 func AuthMiddleware(next http.Handler) http.Handler {
@@ -13,7 +12,7 @@ func AuthMiddleware(next http.Handler) http.Handler {
 		cookie, err := r.Cookie("session_token")
 		if err != nil {
 			if errors.Is(err, http.ErrNoCookie) {
-				handlers.ExecTmpl(w, "web/templates/noLogin.html", nil)
+				http.Redirect(w, r, "/login", http.StatusSeeOther)
 				return
 			}
 			http.Error(w, "Server error", http.StatusInternalServerError)
@@ -25,7 +24,7 @@ func AuthMiddleware(next http.Handler) http.Handler {
 		// Validate JWT
 		claims, err := auth.ValidateJWT(tokenString)
 		if err != nil {
-			handlers.ExecTmpl(w, "web/templates/noLogin.html", nil)
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
 			return
 		}
 
